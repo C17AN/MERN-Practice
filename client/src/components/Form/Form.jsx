b3c3d070f9d5fdcc2c190f60def4871effca1e98
c17an <kimchanmin1@gmail.com>
2021-07-07 15:49:00 +0900

    feat: 챕터 2: CRUD 완성 및 UI 마무리

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from "react";
-import useStyles from "./styles";
+import React, { useState, useEffect } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
 import FileBase from "react-file-base64";
-import { useDispatch } from "react-redux";
-import { createPost } from "../../actions/posts";
 
-const Form = () => {
-  const classes = useStyles();
-  const dispatch = useDispatch();
+import useStyles from "./styles";
+import { createPost, updatePost } from "../../actions/posts";
+
+const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
     creator: "",
     title: "",
@@ -15,13 +14,32 @@ const Form = () => {
     tags: "",
     selectedFile: "",
   });
+  const post = useSelector((state) =>
+    currentId ? state.posts.find((message) => message._id === currentId) : null
+  );
+  const dispatch = useDispatch();
+  const classes = useStyles();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(createPost(postData));
+  useEffect(() => {
+    if (post) setPostData(post);
+  }, [post]);
+
+  const clear = () => {
+    setCurrentId(0);
+    setPostData({ creator: "", title: "", message: "", tags: "", selectedFile: "" });
   };
 
-  const clear = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (currentId === 0) {
+      dispatch(createPost(postData));
+      clear();
+    } else {
+      dispatch(updatePost(currentId, postData));
+      clear();
+    }
+  };
 
   return (
     <Paper className={classes.paper}>
@@ -31,16 +49,16 @@ const Form = () => {
         className={`${classes.root} ${classes.form}`}
         onSubmit={handleSubmit}
       >
-        <Typography variant="h6">Creating a memory</Typography>
+        <Typography variant="h6">
+          {currentId ? `Editing "${post.title}"` : "Creating a Memory"}
+        </Typography>
         <TextField
           name="creator"
           variant="outlined"
           label="Creator"
           fullWidth
           value={postData.creator}
-          onChange={(e) => {
-            setPostData({ ...postData, creator: e.target.value });
-          }}
+          onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
         />
         <TextField
           name="title"
@@ -48,36 +66,32 @@ const Form = () => {
           label="Title"
           fullWidth
           value={postData.title}
-          onChange={(e) => {
-            setPostData({ ...postData, title: e.target.value });
-          }}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
         />
         <TextField
           name="message"
           variant="outlined"
           label="Message"
           fullWidth
+          multiline
+          rows={4}
           value={postData.message}
-          onChange={(e) => {
-            setPostData({ ...postData, message: e.target.value });
-          }}
+          onChange={(e) => setPostData({ ...postData, message: e.target.value })}
         />
         <TextField
           name="tags"
           variant="outlined"
-          label="Tags"
+          label="Tags (coma separated)"
           fullWidth
-          value={postData.text}
-          onChange={(e) => {
-            setPostData({ ...postData, tags: e.target.value });
-          }}
+          value={postData.tags}
+          onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(",") })}
         />
         <div className={classes.fileInput}>
           <FileBase
-            type="File"
+            type="file"
             multiple={false}
             onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })}
-          ></FileBase>
+          />
         </div>
         <Button
           className={classes.buttonSubmit}
--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers/index";
 import postReducer from "./reducers/posts";
+import "./index.css";
 
 import App from "./App";
 
